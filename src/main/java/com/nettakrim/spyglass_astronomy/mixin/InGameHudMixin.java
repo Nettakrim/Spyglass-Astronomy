5f6123f608db0f05146e381765e84effbac22bba
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2024-11-07 14:16:32 +0000

    fixed #38

--- src/main/java/com/nettakrim/spyglass_astronomy/mixin/InGameHudMixin.java
+++ src/main/java/com/nettakrim/spyglass_astronomy/mixin/InGameHudMixin.java
@@ -1,5 +1,6 @@
 package com.nettakrim.spyglass_astronomy.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import com.nettakrim.spyglass_astronomy.SpyglassAstronomyClient;
 
 import net.minecraft.client.gui.DrawContext;
@@ -10,9 +11,8 @@ import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.util.function.Function;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
@@ -21,11 +21,10 @@ public class InGameHudMixin {
     @Unique
     private static final Identifier STAR_SPYGLASS_SCOPE = Identifier.of(SpyglassAstronomyClient.MODID,"textures/star_spyglass_scope.png");
 
-    @Redirect(method = "renderSpyglassOverlay",at = @At(value = "INVOKE",target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIFFIIII)V"))
-    public void swapTexture(DrawContext instance, Function<Identifier, RenderLayer> renderLayers, Identifier sprite, int x, int y, float u, float v, int width, int height, int textureWidth, int textureHeight){
+    @Inject(method = "renderSpyglassOverlay",at = @At(value = "INVOKE",target = "Lnet/minecraft/client/gui/DrawContext;fill(Lnet/minecraft/client/render/RenderLayer;IIIIII)V",ordinal = 0))
+    public void renderSpyglassMode(DrawContext context, float scale, CallbackInfo ci, @Local(name = "k") int k, @Local(name = "l") int l, @Local(name = "i") int i, @Local(name = "j") int j){
         if (SpyglassAstronomyClient.editMode != 0) {
-            sprite = SpyglassAstronomyClient.editMode == 1 ? CONSTELLATION_SPYGLASS_SCOPE : STAR_SPYGLASS_SCOPE;
+            context.drawTexture(RenderLayer::getGuiTextured, SpyglassAstronomyClient.editMode == 1 ? CONSTELLATION_SPYGLASS_SCOPE : STAR_SPYGLASS_SCOPE, k, l, 0.0F, 0.0F, i, j, i, j);
         }
-        instance.drawTexture(renderLayers, sprite, x, y, u, v, width, height, textureWidth, textureHeight);
     }
 }
